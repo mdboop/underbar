9e8c09895f63b78b64369b90f818071d98f719cd
Matthew Brooks <mdeanbrooks@gmail.com>
2015-06-28 23:44:24 -0700

    Implement map and reduce

--- src/underbar.js
+++ src/underbar.js
@@ -117,6 +117,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(value) {
+      result.push(iterator(value));
+    });
+    return result;
   };
 
   /*
@@ -158,6 +163,19 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if(accumulator || accumulator === 0) {
+      _.each(collection, function(value) {
+          accumulator = iterator(accumulator, value);
+        });
+        return accumulator;
+    } else {
+      var workArray = collection.slice(0);
+      accumulator = workArray.shift();
+      _.each(workArray, function(value) {
+        accumulator = iterator(accumulator, value);
+      });
+      return accumulator;
+    }
   };
 
   // Determine if the array or object contains a given value (using `===`).
